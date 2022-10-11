f582dd736863d74420a462cefe06692daae72a2a
take0x <89313929+take0x@users.noreply.github.com>
2022-10-11 17:15:45 +0900

    セキュリティの認証処理を実装する

--- src/main/java/oit/is/z1412/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z1412/kaizi/janken/controller/JankenController.java
@@ -5,35 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.ModelMap;
-// import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class JankenController {
 
-  @GetMapping("/janken.html")
+  @GetMapping("/janken")
   public String janken() {
     return "janken.html";
   }
-
-  /* @GetMapping("/janken.html/{hand}")
-  public String buttle(@PathVariable String hand, ModelMap model) {
-    String player = "";
-    String result = "";
-    if (hand.equals("g")) {
-      player = "グー";
-      result = "Draw!";
-    } else if (hand.equals("c")) {
-      player = "チョキ";
-      result = "You Lose!";
-    } else if (hand.equals("p")) {
-      player = "パー";
-      result = "You Win!";
-    }
-    model.addAttribute("player", "あなたの手: " + player);
-    model.addAttribute("cpu", "相手の手: グー");
-    model.addAttribute("result", "結果: " + result);
-    return "janken.html";
-  } */
 
   @GetMapping("/jankengame")
   public String buttle(@RequestParam String hand, ModelMap model) {
@@ -55,7 +34,7 @@ public class JankenController {
     return "janken.html";
   }
 
-  @PostMapping("/janken.html")
+  @PostMapping("/janken")
   public String janken(@RequestParam String user, ModelMap model) {
     model.addAttribute("welcome", "Hi " + user);
     return "janken.html";
