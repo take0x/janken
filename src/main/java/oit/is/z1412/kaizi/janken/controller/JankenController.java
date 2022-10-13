1ae5f0974687f14dcc7a83a00816e99d19b2c253
take0x <89313929+take0x@users.noreply.github.com>
2022-10-13 14:19:43 +0900

    ログインユーザ名共有機能の実装

--- src/main/java/oit/is/z1412/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z1412/kaizi/janken/controller/JankenController.java
@@ -1,16 +1,28 @@
 package oit.is.z1412.kaizi.janken.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.ModelMap;
 
+import oit.is.z1412.kaizi.janken.model.Entry;
+
 @Controller
 public class JankenController {
 
+  @Autowired
+  private Entry entry;
+
   @GetMapping("/janken")
-  public String janken() {
+  public String janken(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.entry.addUser(loginUser);
+    model.addAttribute("entry", this.entry);
+    model.addAttribute("user", loginUser);
     return "janken.html";
   }
 
@@ -36,7 +48,7 @@ public class JankenController {
 
   @PostMapping("/janken")
   public String janken(@RequestParam String user, ModelMap model) {
-    model.addAttribute("welcome", "Hi " + user);
+    model.addAttribute("user", user);
     return "janken.html";
   }
 }
